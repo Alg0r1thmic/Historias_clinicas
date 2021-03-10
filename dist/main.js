ebe63746aa6d184fa68f80207fdd71ee716395a6
Raul <rquispeto@unsa.edu.pe>
2021-03-09 22:05:36 -0500

    feat: fix

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
+const morgan = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
 async function bootstrap() {
     const options = new swagger_1.DocumentBuilder()
         .setTitle('Historias Clinicas')
@@ -10,6 +13,10 @@ async function bootstrap() {
         .setVersion('1.0')
         .build();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
+    app.use(morgan('dev'));
+    app.use(compression());
+    app.use(helmet());
     const document = swagger_1.SwaggerModule.createDocument(app, options);
     swagger_1.SwaggerModule.setup('docs', app, document);
     await app.listen(process.env.PORT || 3000);
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
-  
-  
+
+
 
 
   const options = new DocumentBuilder()
-  .setTitle('Historias Clinicas')
-  .setDescription('API Rest para historias clinicas')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Historias Clinicas')
+    .setDescription('API Rest para historias clinicas')
+    .setVersion('1.0')
+    .build();
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(morgan('dev'));
+  app.use(compression());
+  app.use(helmet());
+
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT || 3000);
